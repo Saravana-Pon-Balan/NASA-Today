7903969e396c3146f1e59c4c542f9d96014fa215
Saravana <100523948+Saravana-Pon-Balan@users.noreply.github.com>
2024-02-11 09:08:55 +0530

    Delete NASA directory

--- NASA/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NasaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NASA'
--- NASA/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class store(models.Model):
-	title = models.CharField(max_length=100,unique=True)
-	description = models.TextField()
-	url = models.CharField(max_length=300, default="")
-	
-	def __str__(self):
-		return self.title
-
